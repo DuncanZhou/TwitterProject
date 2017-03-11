db89adabd7bc3fd9da48102306bf5d60bd3a26ca
DuncanZhou <364451395@qq.com>
2017-03-11 09:18:19 +0800

    the script ran 4 hous and then it stoped.I have correct some method. Next,i will try to use concurrent method to crawl.

--- GetCNNNewsDataSet/TestGetDataSet.py
+++ GetCNNNewsDataSet/TestGetDataSet.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
 '''@author:duncan'''
+import time
 import json
 from bs4 import BeautifulSoup
 import urllib2
+import os
+import re
 # test religion
 class CNNNewsSpider():
     name = "CNNNewsSpider"
     allowed_domains = ["edition.cnn.com"]
     categories = ["politics","religion","economy","military","education","technology","sports","entertainment","agriculture","money"]
-    start_urls = []
     url_prefix = "http://edition.cnn.com"
 
     def generateAddress(self,page,start,category):
@@ -23,7 +25,7 @@ class CNNNewsSpider():
             # every category with 1000 pages i.e. every category has 10000 text
             caAddress = []
             # i from 1 to 1000
-            for i in range(1,3):
+            for i in range(1,1001):
                 url = self.generateAddress(i,(10 * (i - 1) + 1),ca)
                 caAddress.append(url)
             category[ca] = caAddress
@@ -41,7 +43,10 @@ class CNNNewsSpider():
         jsondata = json.loads(originjson.encode('utf-8'))
         results = jsondata["results"]
         for data in results[0]:
-            url = self.url_prefix + data["url"]
+            if re.match(r"^http",data["url"]) == None:
+                url = self.url_prefix + data["url"]
+            else:
+                url = data["url"]
             urls.append(url)
         return urls
 
@@ -49,6 +54,7 @@ class CNNNewsSpider():
     def generateCategoryUrls(self,pageurl):
         categoryUrl = {}
         for ca in self.categories:
+            id = 0
             urls = []
             # pageurls is a list-type
             pageurls = pageurl[ca]
@@ -58,35 +64,85 @@ class CNNNewsSpider():
                     urls += texturls
                 except Exception as e:
                     pass
+                id += 1
+                print "process %d urls" % id
             categoryUrl[ca] = urls
-            print "finished category %s" % ca
-
-        with open("category-urls","wb") as f:
-            for key in categoryUrl.keys():
-                print len(categoryUrl[key])
-                f.write(key)
-                f.write("\t")
-                for url in categoryUrl[key]:
-                    if url != None:
-                        f.write(url) + f.write("\t")
+            urls_path = "/home/duncan/CNNNewsDataSet/"
+            # 把获取得到的文本页面链接存入
+            with open(urls_path + ca + "texturls","wb") as f:
+                for url in urls:
+                    f.write(url)
+                    f.write("\n")
+            print "finished category urls %s" % ca
         return categoryUrl
 
     # get the text in one page
     def getText(self,url):
         text = ""
-        html = urllib2.urlopen(url).read()
-        soup = BeautifulSoup(html,"lxml")
-        result = soup.find_all(attrs={"class":"zn-body__paragraph"})
-        for res in result:
-            text += res.string
+        # 只抓取以html结尾的网页
+        if len(re.findall(r".html$",url)) == 0:
+            return text
+        try:
+            html = urllib2.urlopen(url).read()
+            soup = BeautifulSoup(html,"lxml")
+            result = soup.find_all(attrs={"class":"zn-body__paragraph"})
+            for res in result:
+                if res.string != None:
+                    text += res.string
+        except Exception as e:
+            pass
+        return text
+
+    # categoryUrl is a dictionary-type
+    def DownloadText(self,categoryUrl):
+        for category in categoryUrl.keys():
+            # 创建类别的文本文件夹
+            if os.path.exists("/home/duncan/CNNNewsDataSet/" + category + "Text") == False:
+                os.mkdir("/home/duncan/CNNNewsDataSet/" + category + "Text")
+            categoryText_foler_path = "/home/duncan/CNNNewsDataSet/" + category + "Text/"
+            texturls = categoryUrl[category]
+            id = 1
+            for url in texturls:
+                text = self.getText(url)
+                if text != "":
+                    # 写入文件
+                    with open(categoryText_foler_path + str(id),"w") as f:
+                        f.write(text.encode("utf-8"))
+                    id += 1
+            print "finished fetching %s category text" % category
+
+    # 从一个url中下载文本
+    def DownloadOnePageText(self,url):
+        text = ""
+        try:
+            text = self.getText(url)
+        except Exception as e:
+            pass
         return text
 
 if __name__ == '__main__':
+    starttime = time.time()
     spider = CNNNewsSpider()
-    pageurl = spider.generatePageUrls()
-    # print pageurl
-    spider.generateCategoryUrls(pageurl)
-# spider.getOnePageURLs("http://searchapp.cnn.com/search/query.jsp?page=3&npp=10start=21&text=health&type=all&bucket=true&sort=relevance")
+    # pageurl = spider.generatePageUrls()
+    # categoryUrl = spider.generateCategoryUrls(pageurl)
+    # spider.DownloadText(categoryUrl)
+
+    # test getting text
+    with open("/home/duncan/CNNNewsDataSet/religiontexturls","r") as f:
+        urls = f.readlines()
+        id = 1
+        for url in urls:
+            text = spider.DownloadOnePageText(url)
+            if text == "":
+                continue
+            # 写入文件
+            with open("/home/duncan/CNNNewsDataSet/religionText/" + str(id),"w") as f:
+                f.write(text.encode("utf-8"))
+            id += 1
+            print "已写入%d个文本" % id
+    endtime = time.time()
+    print "cost %f s" % (endtime - starttime)
+
 
 
 
